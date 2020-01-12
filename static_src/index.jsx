e354192e830e7c404fb38dbe84c187288668aeed
yuri-dmitriev <yuri.dmitriev@list.ru>
2020-01-12 04:55:36 +0300

    Add a send message button

--- static_src/index.jsx
+++ static_src/index.jsx
@@ -4,12 +4,35 @@ import './style.css';
 
 let messages = ['Привет', 'Как дела?'];
 
-const MessageComponent = (props) => <div className='message'> {props.text} </div>;
+const MessageComponent = (props) => <div className='message'>{props.text}</div>;
+
 const MessageField = (props) => {
-  return props.messages.map((message, idx) => <MessageComponent key={idx} text={message}/>);
+  return props.messages.map(message => <MessageComponent text={ message }/>);
+};
+
+const SendMessageButton = () => {
+  function handleClick() {
+    messages.push('Нормально');
+
+    ReactDOM.render(
+      <App/>,
+      document.getElementById('root')
+    );
+  }
+
+  return <button onClick={handleClick}>Отправить</button>;
+};
+
+const App = () => {
+  return (
+    <div>
+      <MessageField messages={ messages }/>
+      <SendMessageButton/>
+    </div>
+  );
 };
 
 ReactDOM.render(
-  <MessageField messages={messages}/>,
-  document.getElementById('root'),
+  <App/>,
+  document.getElementById('root')
 );
